319f6ecd75fc63d3c43fbace1862b3c35f4d5e80
Abhinay S <145796485+Abhinay25670@users.noreply.github.com>
2026-02-23 17:18:21 +0530

    All files uploaded

---
+++ model.py
@@ -0,0 +1,48 @@
+from sklearn.svm import LinearSVC
+import numpy as np
+import cv2 as cv
+import PIL
+
+class Model:
+
+    def __init__(self):
+        self.model = LinearSVC()
+
+    def train_model(self, counters):
+        img_list = np.array([])
+        class_list = np.array([])
+
+        img_size = (160,120)
+
+        for i in range(1, counters[0]):
+            img = cv.imread(f'1/frame{i}.jpg')[:, :, 0]
+            img=cv.resize(img,img_size)
+            img = img.reshape(19200)
+            img_list = np.append(img_list, [img])
+            class_list = np.append(class_list, 1)
+
+        for i in range(1, counters[1]):
+            img = cv.imread(f'2/frame{i}.jpg')[:, :, 0]
+            img=cv.resize(img,img_size)
+            img = img.reshape(19200)
+            img_list = np.append(img_list, [img])
+            class_list = np.append(class_list, 2)
+
+        img_list = img_list.reshape(counters[0] - 1 + counters[1] - 1, 19200)
+        self.model.fit(img_list, class_list)
+        print("Model successfully trained!")
+
+    def predict(self, frame):
+        img_size = (160,120)
+        frame = frame[1]
+        cv.imwrite("frame.jpg", cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
+        img = PIL.Image.open("frame.jpg")
+        img.thumbnail((150, 150), PIL.Image.Resampling.LANCZOS)
+        img.save("frame.jpg")
+
+        img = cv.imread('frame.jpg')[:, :, 0]
+        img = cv.resize(img, img_size)
+        img = img.reshape(19200)
+        prediction = self.model.predict([img])
+
+        return prediction[0]
